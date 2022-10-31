ce2331444634f7ee5dc3944b2feebbcea049219f
victordhare1 <victordhare12@gmail.com>
2022-10-31 14:04:15 -0700

    commit

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -4,11 +4,8 @@
   * set_string - Sets the value of a pointer to char
   * @s: the value to modify
   * @to: the value to assign
-  *
-  * Return: Nothing.
   */
 void set_string(char **s, char *to)
 {
 	*s = to;
 }
-
