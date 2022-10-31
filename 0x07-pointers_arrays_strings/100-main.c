434335bfaa46ad0077dbd69cf9c6f0d901b7f469
victordhare1 <victordhare12@gmail.com>
2022-10-31 14:29:35 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/100-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+ char *s0 = "Bob Dylan";
+ char *s1 = "Robert Allen";
+
+ printf("%s, %s\n", s0, s1);
+ cap_string(char *);
+ printf("%s, %s\n", s0, s1);
+ return (0);
+}
