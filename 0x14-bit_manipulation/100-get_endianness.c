78d2da2b573bb459b8e7cd9d1c8c87d663b163e8
victordhare1 <victordhare12@gmail.com>
2022-12-01 10:38:57 -0800

    commit

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,18 +1,14 @@
 #include "main.h"
-
 /**
- * get_endianness - checks if multibyte data is stored as little or big endian
- * Return: first value stored, 0 if big endian, 1 if little
+ * get_endianness - defines if a pc is big or little endianness.
+ * Return: 0 big 1 little
  */
-
 int get_endianness(void)
 {
-	/* have a 4 byte int hold some hex number 0x00000001 */
-	/* test if first byte stored is 00 or 01 */
-	/* big endian would store 00, little would store 01 */
+	int i = 1;
 
-	int n = 0x00000001;
-	char *c = (char *)&n;
-
-	return (c[0]);
+	if ((i >> 31) == 1)
+		return (0);
+	else
+		return (1);
 }
